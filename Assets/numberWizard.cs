1c14a4ea955ae3b2b1820456cbcb84f5b003d213
Matthew Leong <mattleong5@gmail.com>
2018-07-19 14:11:08 -0400

    Finished number wizard challenge from unity course

--- Assets/numberWizard.cs
+++ Assets/numberWizard.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class numberWizard : MonoBehaviour {
 
-    int guessMax = 1000;
-    int guessMin = 1;
-    int guess = 500;
+    int guessMax;
+    int guessMin;
+    int guess;
 
     // Use this for initialization
     void Start () {
@@ -16,12 +16,16 @@ public class numberWizard : MonoBehaviour {
 
     void StartGame()
     {
+        guessMax = 1000;
+        guessMin = 1;
+        guess = 500;
         Debug.Log("Welcome to Number Wizard");
         print("Please pick a number");
         print("The highest number is " + guessMax);
         print("The lowest number is " + guessMin);
         print("Is number higher or lower than my 500");
         print("Push up = higher, push down = lower, push enter = correct");
+        
         guessMax += 1;
     }
 
@@ -30,19 +34,25 @@ public class numberWizard : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             guessMin = guess;
-            guess = (guessMax + guessMin) / 2;
-            print("Up arrow pressed");
+            NextGuess();
             print(guessMin);
             print(guess);
         } else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             guessMax = guess;
-            guess = (guessMax + guessMin) / 2;
-            print("Down arrow pressed");
+            NextGuess();
             print(guessMax);
         } else if (Input.GetKeyDown(KeyCode.Return))
         {
-            print("Enter button pressed");
+            print("I am correct!");
+            print("Lets play again!");
+            StartGame();
         }
 	}
+
+    void NextGuess()
+    {
+        guess = (guessMax + guessMin) / 2;
+        print("It is higher than or lower than " + guess);
+    }
 }
